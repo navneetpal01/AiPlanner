2afc61dc336ae3fb71d567e8eb83051cf85ef9ea
navneetpal01 <navneetpalgithub@gmail.com>
2024-02-22 16:44:16 +0530

    22feb

--- app/src/main/java/com/example/aiplanner/presentation/onboarding/components/onboardingphone/OnBoardingPhoneFrame.kt
+++ app/src/main/java/com/example/aiplanner/presentation/onboarding/components/onboardingphone/OnBoardingPhoneFrame.kt
@@ -1,6 +1,5 @@
 package com.example.aiplanner.presentation.onboarding.components.onboardingphone
 
-import android.util.Log
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
